eb86c4ada1803eb16119788793f1bb3b695f1c8c
tianyi <shty1990@163.com>
2019-01-24 17:44:03 +0800

    export hash

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // import 'babel-polyfill';
 import Jsonrpc from './jsonrpc';
 import Wallet from './wallet';
+import hash from './crypto/hash';
 
-export { Jsonrpc, Wallet };
+export { Jsonrpc, Wallet, hash };
