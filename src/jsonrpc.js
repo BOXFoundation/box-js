efa6f6fcf66bcd306335161e269e1d3331858b1f
yw8820 <wen.yi@castbox.fm>
2019-01-25 14:34:27 +0800

    fix bug

--- src/jsonrpc.js
+++ src/jsonrpc.js
@@ -130,11 +130,11 @@ export default class JsonRpc {
   listTransactions = async addr => {
     return await this.fetch('/wlt/listtransactions', { addr });
   };
-}
-
-/** 
+  /** 
  * getTransactionByHash
 */
   getTransactionByHash = async hash => {
     return await this.fetch('/v1/webapi/gettransaction', { hash });
   }
+}
+
