9df41e618e76177c1fb046d406093cc6cdcec7de
tianyi <shty1990@163.com>
2019-01-30 15:25:03 +0800

    change test data

--- test/proto.test.js
+++ test/proto.test.js
@@ -3,20 +3,20 @@ const { hash256 } = require('../src/crypto/hash');
 
 const testTx = {
   tx: {
-    version: 0,
-    vin: [
+    Version: 0,
+    Vin: [
       {
-        prevOutPoint: {
-          hash:
+        PrevOutPoint: {
+          Hash:
             'e4277a20dc9dacb4ff907c9eb6875e4dc0478156166d2aa3177c935b4b7a9308',
-          index: 0
+          Index: 0
         },
-        scriptSig:
+        ScriptSig:
           'RjBEAiBVdkxaNams5NUNhCgeHcq1k9wtG7ck4pk/uQ5MTHa7GQIgEz+8DB9ZRXPlBj3UfmmtxztWWAsMjacy+gBeEJtTvyAhAnnL2P9ieAgsPd1XEt09fcaDJlr9WFrqwlA9OkhdMA5V',
-        sequence: 0
+        Sequence: 0
       }
     ],
-    vout: [
+    Vout: [
       {
         value: 4294,
         script_pub_key: 'dqkUsqcHHH3pTFrF382HYjL5vOiyo06IrA=='
@@ -38,9 +38,9 @@ const testTx = {
         script_pub_key: 'dqkUsqcHHH3pTFrF382HYjL5vOiyo06IrA=='
       }
     ],
-    data: null,
-    magic: 0,
-    lockTime: 0
+    Data: null,
+    Magic: 0,
+    LockTime: 0
   },
   hash: '1f63ab208e4df84bc62fd14bb0fb0b0d8358b16e47351344e5703a91c4bba218'
 };
@@ -68,7 +68,9 @@ const text = async () => {
         '128f010a220a2008937a4b5b937c17a32a6d16568147c04d5e87b69e7c90ffb4ac9ddc207a27e41269463044022055764c5a35a9ace4d50d84281e1dcab593dc2d1bb724e2993fb90e4c4c76bb190220133fbc0c1f594573e5063dd47e69adc73b56580b0c8da732fa005e109b53bf20210279cbd8ff6278082c3ddd5712dd3d7dc683265afd585aeac2503d3a485d300e551a1e08c621121976a914b2a7071c7de94c5ac5dfcd876232f9bce8b2a34e88ac1a1e088d34121976a9141f63c29a18fa38cd5e1b53d3a825e0aa05ec35e988ac1a1e08c02d121976a9143dd2211a357141386e24b13f39c5d9c55ddc1ebd88ac1a1e088c2d121976a91488356ff5e504d0552b56cb1520ca855754824ae588ac1a1f089ef204121976a914b2a7071c7de94c5ac5dfcd876232f9bce8b2a34e88ac',
         'hex'
       )
-    ).reverse().toString('hex')
+    )
+      .reverse()
+      .toString('hex')
   );
 };
 text();
