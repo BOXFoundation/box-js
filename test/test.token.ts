399a8270fad529f6100f7e7630f604127de1c059
wangchen <jimmychen21@163.com>
2019-04-28 16:40:13 +0800

    修改：package.json

--- test/test.token.ts
+++ test/test.token.ts
@@ -1,5 +1,5 @@
-/* import 'mocha'
-import should from 'should' */
+import { expect } from 'chai'
+import 'mocha'
 import {
   encodeTokenAddr,
   decodeTokenAddr
@@ -10,20 +10,16 @@ const _token_addr = '8b9aLjo62TiDWr3JoszMWYAQH5iEvJFKyNZJQ73ghywJC7ACVHh',
     'e26dc08ca79a9a07a82e2cef5350ae74f2590385fa1d316a5c7e9500c749ab71',
   _token_index = 0
 
-const testEncodeTokenAddr = async () => {
-  const token_addr = await encodeTokenAddr(_token_hash, _token_index)
-  console.log('[test.token] EncodeTokenAddr:', token_addr === _token_addr)
-  return token_addr
-}
-
-const testDecodeTokenAddr = async () => {
-  const token_addr = await testEncodeTokenAddr()
-  const { opHash, index } = await decodeTokenAddr(token_addr)
-  console.log(
-    '[test.token] DecodeTokenAddr:',
-    opHash === _token_hash,
-    index === _token_index
-  )
-}
-
-testDecodeTokenAddr()
+describe('[test.token]', async () => {
+  // test func [testEncodeTokenAddr]
+  it('Encode Token Address', async () => {
+    const token_addr = await encodeTokenAddr(_token_hash, _token_index)
+    expect(token_addr).to.equal(_token_addr)
+  })
+  // test func [testEncodeTokenAddr]
+  it('Decode Token Address', async () => {
+    const { opHash, index } = await decodeTokenAddr(_token_addr)
+    expect(opHash).to.equal(_token_hash)
+    expect(index).to.equal(_token_index)
+  })
+})
