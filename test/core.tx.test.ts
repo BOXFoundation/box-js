4f9f8a7dd69c6374bec05ae773a81f70037d867c
wangchen <jimmychen21@163.com>
2019-05-27 14:50:14 +0800

    创建：account_4

--- test/core.tx.test.ts
+++ test/core.tx.test.ts
@@ -23,39 +23,6 @@ test('Make a BOX Transaction', async () => {
   expect(tx_detail.detail.hash).toEqual(tx_result.hash)
 })
 
-/* test('Sign Transaction by PrivKey or Crypto', async () => {
-  await cor
-    .makeUnsignedTx({
-      from: Data.acc_addr_3,
-      to: Data.to_addrs,
-      amounts: Data.amounts,
-      fee: Data.fee
-    })
-    .then(async res => {
-      // console.log('unsigned_tx:', JSON.stringify(res))
-      const signed_tx = await cor.signTxByPrivKey({
-        unsignedTx: {
-          tx: res.tx,
-          rawMsgs: res.rawMsgs
-        },
-        privKey: Data.acc_privateKey_3
-      })
-      const signed_tx_acc = await feature.signTxByCrypto({
-        unsignedTx: {
-          tx: res.tx,
-          rawMsgs: res.rawMsgs
-        },
-        crypto: Keystore.keystore_3,
-        pwd: Data.acc_pwd
-      })
-      expect(signed_tx).toEqual(signed_tx_acc)
-    })
-    .catch(err => {
-      console.error('Sign Transaction by PrivKey or Crypto Error:', err)
-      expect(0).toBe(1)
-    })
-}) */
-
 test('Get the BOX Balance of the given Address', async () => {
   await cor
     .getBalance(Data.acc_addr_3)
@@ -87,6 +54,39 @@ test('Get the BOX Balances of the given Addresses', async () => {
     amount: 30000000000
   })
   console.log('faucet res:', faucet_res)
+}) */
+
+/* test('Sign Transaction by PrivKey or Crypto', async () => {
+  await cor
+    .makeUnsignedTx({
+      from: Data.acc_addr_3,
+      to: Data.to_addrs,
+      amounts: Data.amounts,
+      fee: Data.fee
+    })
+    .then(async res => {
+      // console.log('unsigned_tx:', JSON.stringify(res))
+      const signed_tx = await cor.signTxByPrivKey({
+        unsignedTx: {
+          tx: res.tx,
+          rawMsgs: res.rawMsgs
+        },
+        privKey: Data.acc_privateKey_3
+      })
+      const signed_tx_acc = await feature.signTxByCrypto({
+        unsignedTx: {
+          tx: res.tx,
+          rawMsgs: res.rawMsgs
+        },
+        crypto: Keystore.keystore_3,
+        pwd: Data.acc_pwd
+      })
+      expect(signed_tx).toEqual(signed_tx_acc)
+    })
+    .catch(err => {
+      console.error('Sign Transaction by PrivKey or Crypto Error:', err)
+      expect(0).toBe(1)
+    })
 }) */
 
 // todo
