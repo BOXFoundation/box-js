aeda4141db51386b4b106326277e78bc1921bae8
wangchen <jimmychen21@163.com>
2019-08-02 17:43:21 +0800

    create : split/util

---
+++ src/boxd/core/split/util.ts
@@ -0,0 +1,35 @@
+import CommonUtil from '../../util/util'
+import Account from '../../account/account'
+import Hash from '../../util/crypto/hash'
+
+namespace Util {
+  export const calcSplitAddr = (
+    addrs: string[],
+    weights: number[],
+    txHash: string,
+    index: number
+  ) => {
+    if (addrs.length !== weights.length) {
+      throw new Error(`Address count doesn't match weight count`)
+    }
+
+    const op = new CommonUtil.Opcoder('')
+    for (let i = 0; i < addrs.length; i++) {
+      const weight = CommonUtil.putUint32(Buffer.alloc(4), weights[i])
+      const pkh = Buffer.from(
+        new Account().dumpPubKeyHashFromAddr(addrs[i]),
+        'hex'
+      )
+      op.add(pkh).add(weight)
+    }
+
+    const splitHashBs = Hash.ripemd160(Hash.sha256(op.getCode()))
+    const idxBytes = CommonUtil.putUint32(Buffer.alloc(4), index)
+    const hashBytes = Buffer.from(txHash, 'hex')
+    const raw = Buffer.concat([hashBytes, idxBytes, splitHashBs])
+
+    return Hash.ripemd160(Hash.sha256(raw)).toString('hex')
+  }
+}
+
+export default Util
