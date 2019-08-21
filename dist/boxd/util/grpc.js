9481796a9c871e60894cb2ba56d0ee2174c82195
wangchen <jimmychen21@163.com>
2019-08-21 13:08:19 +0800

    hide grpc

--- dist/boxd/util/grpc.js
+++ dist/boxd/util/grpc.js
@@ -1,6 +1,5 @@
 // import grpc from 'grpc'
 // import protoLoader from '@grpc/proto-loader'
-
 // /* load protobuf file */
 // const packageDefinition = protoLoader.loadSync('../../protobuf/web.proto', {
 //   keepCase: true,
@@ -10,39 +9,31 @@
 //   oneofs: true
 // })
 // var hello_proto = grpc.loadPackageDefinition(packageDefinition).rpcpb
-
 // // console.log('hello_proto.WebApi :', hello_proto.WebApi)
-
 // export default class Grpc {
 //   protected path
-
 //   public constructor(path) {
 //     this.path = path
 //   }
-
 //   public connect(param) {
 //     // make client
 //     const client = new hello_proto.WebApi(
 //       this.path,
 //       grpc.credentials.createInsecure()
 //     )
-
 //     // connection
 //     return new Promise(resolve => {
 //       const stream = client.Connect()
 //       // console.log('stream :', stream)
-
 //       // connecting
 //       stream.on('data', data => {
 //         console.log('[Connect] Data :', data)
 //       })
-
 //       // ended
 //       stream.on('end', () => {
 //         console.log('[Connect] End')
 //         resolve()
 //       })
-
 //       // try
 //       stream.write(param)
 //     })
--- src/boxd/boxd.ts
+++ src/boxd/boxd.ts
@@ -3,14 +3,14 @@ import AccountManager from './account/account-manager'
 import Api from './core/api'
 import Feature from './core/feature'
 import Util from '../boxd/util/util'
-import Grpc from '../boxd/util/grpc'
+// import Grpc from '../boxd/util/grpc'
 
 const boxd = {
   Account,
   AccountManager,
   Api,
   Feature,
-  Grpc,
+  // Grpc,
   Util
 }
 
